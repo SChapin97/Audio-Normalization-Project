46942489034f9afc03e781989948da96a63d6476
SChapin97 <samuelchapin97@gmail.com>
2018-11-08 13:04:49 -0600

    Initialization of the backend classes.

---
+++ src/backend/Normalizer.java
@@ -0,0 +1,11 @@
+package backend;
+
+import javax.sound.sampled.*;
+/*This is the Java Sound API, which handles audio files, audio streams, and MIDI devices.
+ *The sampled package in the Sound API handles audio files and audio streams while the midi package handles MIDI i/o.
+ *The sampled package mainly handles real-time audio, but is used for audio file manipulation in this case.
+*/
+
+public class Normalizer {
+
+}//end of Normalize class
